9b9f7944fefcce5e534fe17f26c6eb1036817870
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-24 18:56:52 -0300

    feat: add initial add product button component

---
+++ app/products/_components/add-product-button.tsx
@@ -0,0 +1,149 @@
+'use client'
+
+import { Button } from '@/app/_components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/app/_components/ui/dialog'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { PlusIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/app/_components/ui/form'
+import { Input } from '@/app/_components/ui/input'
+import { NumericFormat } from 'react-number-format'
+
+const addProductSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: 'O nome do produto é obrigatorio',
+  }),
+  price: z.number().min(0.01, {
+    message: 'O valor do produto deve ser maior que zero',
+  }),
+  stock: z.number().int().min(0, {
+    message: 'O estoque deve ser maior que zero',
+  }),
+})
+
+type AddProductSchema = z.infer<typeof addProductSchema>
+
+const AddProductButton = () => {
+  const form = useForm<AddProductSchema>({
+    shouldUnregister: true,
+    resolver: zodResolver(addProductSchema),
+    defaultValues: {
+      name: '',
+      price: 0,
+      stock: 1,
+    },
+  })
+
+  const onSubmit = (data: AddProductSchema) => {
+    console.log(data)
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo produto
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <DialogHeader className="flex items-center justify-center">
+              <DialogTitle>Criar produto</DialogTitle>
+              <DialogDescription>
+                Insira as informações abaixo
+              </DialogDescription>
+            </DialogHeader>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome do produto" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Preço</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      thousandSeparator="."
+                      decimalSeparator=","
+                      fixedDecimalScale
+                      decimalScale={2}
+                      prefix="R$ "
+                      allowNegative={false}
+                      customInput={Input}
+                      onValueChange={(values) =>
+                        field.onChange(values.floatValue)
+                      }
+                      {...field}
+                      onChange={() => {}}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Estoque</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Digite o estoque do produto"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary" type="reset">
+                  Cancelar
+                </Button>
+              </DialogClose>
+              <Button type="submit">Salvar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default AddProductButton
