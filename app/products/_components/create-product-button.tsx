0edca4e3165692fb4da8ede1fbfafd73cefae5e8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-28 17:51:59 -0300

    refactor: add UpsertProductDialogContent component

--- app/products/_components/create-product-button.tsx
+++ app/products/_components/create-product-button.tsx
@@ -1,59 +1,14 @@
 'use client'
 
 import { Button } from '@/app/_components/ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/app/_components/ui/dialog'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Loader2Icon, PlusIcon } from 'lucide-react'
-import { useForm } from 'react-hook-form'
+import { Dialog, DialogTrigger } from '@/app/_components/ui/dialog'
+import { PlusIcon } from 'lucide-react'
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/app/_components/ui/form'
-import { Input } from '@/app/_components/ui/input'
-import { NumericFormat } from 'react-number-format'
-import { createProduct } from '@/app/_actions/product/create-product'
 import { useState } from 'react'
-import {
-  createProductSchema,
-  CreateProductSchema,
-} from '@/app/_actions/product/create-product/schema'
+import UpsertProductDialogContent from './upsert-product-dialog'
 
 const CreateProductButton = () => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false)
-  const form = useForm<CreateProductSchema>({
-    shouldUnregister: true,
-    // @ts-expect-error - resolver is defined
-    resolver: zodResolver(createProductSchema),
-    defaultValues: {
-      name: '',
-      price: 0,
-      stock: 1,
-    },
-  })
-
-  const onSubmit = async (data: CreateProductSchema) => {
-    try {
-      await createProduct(data)
-      setDialogIsOpen(false)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
   return (
     <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
       <DialogTrigger asChild>
@@ -62,98 +17,7 @@ const CreateProductButton = () => {
           Novo produto
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <Form {...form}>
-          <form
-            // @ts-expect-error - onSubmit is defined
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
-          >
-            <DialogHeader className="flex items-center justify-center">
-              <DialogTitle>Criar produto</DialogTitle>
-              <DialogDescription>
-                Insira as informações abaixo
-              </DialogDescription>
-            </DialogHeader>
-            <FormField
-              // @ts-expect-error - control is defined
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Digite o nome do produto" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              // @ts-expect-error - control is defined
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Preço</FormLabel>
-                  <FormControl>
-                    <NumericFormat
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      fixedDecimalScale
-                      decimalScale={2}
-                      prefix="R$ "
-                      allowNegative={false}
-                      customInput={Input}
-                      onValueChange={(values) =>
-                        field.onChange(values.floatValue)
-                      }
-                      {...field}
-                      onChange={() => {}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              // @ts-expect-error - control is defined
-              control={form.control}
-              name="stock"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Estoque</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="Digite o estoque do produto"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="secondary" type="reset">
-                  Cancelar
-                </Button>
-              </DialogClose>
-              <Button
-                type="submit"
-                disabled={form.formState.isSubmitting}
-                className="gap-1.5"
-              >
-                {form.formState.isSubmitting && (
-                  <Loader2Icon className="animate-spin" size={18} />
-                )}
-                Salvar
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+      <UpsertProductDialogContent onSuccess={() => setDialogIsOpen(false)} />
     </Dialog>
   )
 }
