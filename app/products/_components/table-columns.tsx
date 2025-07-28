3e90b38c48189e0ba95a4ee39d5399610af6bb76
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-07-28 18:21:02 -0300

    feat: format product unit price as currency

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -35,6 +35,14 @@ export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: 'price',
     header: 'Valor unitário',
+    cell: (row) => {
+      const product = row.row.original
+      return Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+      }).format(Number(product.price))
+    },
   },
   {
     accessorKey: 'stock',
