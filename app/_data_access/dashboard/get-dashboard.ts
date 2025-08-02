225748e3b7b972ba0561f95839cc982d3852a14c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-02 18:27:40 -0300

    feat: add initial revenue chart

--- app/_data_access/dashboard/get-dashboard.ts
+++ app/_data_access/dashboard/get-dashboard.ts
@@ -1,4 +1,12 @@
+import 'server-only'
+
 import { db } from '@/app/_lib/prisma'
+import dayjs from 'dayjs'
+
+export interface DayTotalRevenue {
+  day: string
+  totalRevenue: number
+}
 
 interface DashboardDto {
   totalRevenue: number
@@ -6,25 +14,61 @@ interface DashboardDto {
   totalSales: number
   totalStock: number
   totalProducts: number
+  totalLast14DaysRevenue: DayTotalRevenue[]
 }
 
 export const getDashboard = async (): Promise<DashboardDto> => {
-  const totalRevenuePromise = db.saleProduct.aggregate({
-    _sum: {
-      unitPrice: true,
+  const today = dayjs().endOf('day').toDate()
+  const last14Days = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(
+    (day) => {
+      return dayjs(today).subtract(day, 'day')
     },
-  })
-  const todayRevenuePromise = await db.saleProduct.aggregate({
-    _sum: {
-      unitPrice: true,
-    },
-    where: {
-      createdAt: {
-        gte: new Date(new Date().setHours(0, 0, 0, 0)),
-        lte: new Date(new Date().setHours(23, 59, 59, 999)),
-      },
-    },
-  })
+  )
+
+  const totalLast14DaysRevenue: DayTotalRevenue[] = []
+
+  for (const day of last14Days) {
+    const dayTotalRevenue = await db.$queryRawUnsafe<
+      { totalRevenue: number }[]
+    >(
+      `
+      SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
+      FROM "SaleProduct"
+      WHERE "createdAt" >= $1 AND "createdAt" <= $2;
+      `,
+      day.startOf('day').toDate(),
+      day.endOf('day').toDate(),
+    )
+
+    totalLast14DaysRevenue.push({
+      day: day.format('DD/MM'),
+      totalRevenue: dayTotalRevenue[0]?.totalRevenue ?? 0,
+    })
+  }
+
+  const totalRevenueQuery = `
+    SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
+    FROM "SaleProduct";
+  `
+
+  const todayRevenueQuery = `
+    SELECT SUM("unitPrice" * "quantity") as "todayRevenue"
+    FROM "SaleProduct"
+    WHERE "createdAt" >= $1 AND "createdAt" <= $2;
+  `
+
+  const startOfDay = new Date(new Date().setHours(0, 0, 0, 0))
+  const endOfDay = new Date(new Date().setHours(23, 59, 59, 999))
+
+  const totalRevenuePromise =
+    db.$queryRawUnsafe<{ totalRevenue: number }[]>(totalRevenueQuery)
+
+  const todayRevenuePromise = db.$queryRawUnsafe<{ todayRevenue: number }[]>(
+    todayRevenueQuery,
+    startOfDay,
+    endOfDay,
+  )
+
   const totalSalesPromise = db.sale.count()
   const totalStockPromise = db.product.aggregate({
     _sum: {
@@ -33,19 +77,26 @@ export const getDashboard = async (): Promise<DashboardDto> => {
   })
   const totalProductsPromise = db.product.count()
 
-  const [totalRevenue, todayRevenue, totalSales, totalStock, totalProducts] =
-    await Promise.all([
-      totalRevenuePromise,
-      todayRevenuePromise,
-      totalSalesPromise,
-      totalStockPromise,
-      totalProductsPromise,
-    ])
-  return {
-    totalRevenue: Number(totalRevenue._sum.unitPrice),
-    todayRevenue: Number(todayRevenue._sum.unitPrice),
+  const [
+    totalRevenueResult,
+    todayRevenueResult,
     totalSales,
-    totalStock: totalStock._sum.stock ?? 0,
+    totalStock,
     totalProducts,
+  ] = await Promise.all([
+    totalRevenuePromise,
+    todayRevenuePromise,
+    totalSalesPromise,
+    totalStockPromise,
+    totalProductsPromise,
+  ])
+
+  return {
+    totalRevenue: totalRevenueResult[0]?.totalRevenue ?? 0,
+    todayRevenue: todayRevenueResult[0]?.todayRevenue ?? 0,
+    totalSales,
+    totalStock: Number(totalStock._sum.stock) ?? 0,
+    totalProducts,
+    totalLast14DaysRevenue,
   }
 }
